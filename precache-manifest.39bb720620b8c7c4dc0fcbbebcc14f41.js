d7121d3f4d19ddb8040e4aed93da614212d8e8b0
dogrodnik <d.ogrodnik96@gmail.com>
2019-07-30 20:28:29 +0200

    Updates

---
+++ precache-manifest.39bb720620b8c7c4dc0fcbbebcc14f41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e643c62aa183edb53fa09e6b450da0d",
+    "url": "/guess-word/index.html"
+  },
+  {
+    "revision": "7cf8707b952409df5ce0",
+    "url": "/guess-word/static/css/main.6b2481a3.chunk.css"
+  },
+  {
+    "revision": "135a71bb4a5e23595c8e",
+    "url": "/guess-word/static/js/2.4eff2e9f.chunk.js"
+  },
+  {
+    "revision": "7cf8707b952409df5ce0",
+    "url": "/guess-word/static/js/main.5c0953d4.chunk.js"
+  },
+  {
+    "revision": "3a7e45b18ab9aeed4018",
+    "url": "/guess-word/static/js/runtime~main.c2fffbc3.js"
+  }
+]);
